97159a037bb00ddf42bd29ae90148c7fc6c2c3dc
AdamM <adam@photosynthesis.ca>
2022-01-12 12:36:38 +0100

    License & version update

--- murmurations-react-interfaces.php
+++ murmurations-react-interfaces.php
@@ -1,13 +1,34 @@
 <?php
 /**
- * @wordpress-plugin
- * Plugin Name: Murmurations React Interfaces
+ * Plugin Name:       Murmurations React Interfaces
+ * Plugin URI:        https://murmurations.network
+ * Description:       Client-side interfaces for the Murmurations Aggregator
+ * Version:           0.2.0
+ * Author:            A. McKenty / Photosynthesis
+ * Author URI:        Photosynthesis.ca
+ * License:           GPLv3
+ * License URI:				https://www.gnu.org/licenses/gpl-3.0.html
  */
+
+/*
+The Murmurations React Interfaces plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+Murmurations React Interfaces is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the Murmurations Aggregator plugin. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+*/
 
 defined( 'ABSPATH' ) or die( 'Direct script access disallowed.' );
 
-define( 'ERW_WIDGET_PATH', plugin_dir_path( __FILE__ ) . 'widget' );
-define( 'ERW_ASSET_MANIFEST', ERW_WIDGET_PATH . '/build/asset-manifest.json' );
+define( 'MRI_WIDGET_PATH', plugin_dir_path( __FILE__ ) . 'widget' );
+define( 'MRI_ASSET_MANIFEST', MRI_WIDGET_PATH . '/build/asset-manifest.json' );
 define( 'MRI_INCLUDES', plugin_dir_path( __FILE__ ) . '/includes' );
 
 require_once( MRI_INCLUDES . '/enqueue.php' );
